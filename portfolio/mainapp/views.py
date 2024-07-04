2f844deb8fe1365bc0b56300d667633724520856
Neal L <rlawton.cs@gmail.com>
2024-07-03 20:37:27 -0700

    Wrote the unique content for the home.html template.

--- portfolio/mainapp/views.py
+++ portfolio/mainapp/views.py
@@ -6,8 +6,14 @@ from .models import Project, Tag
 
 # This view takes in a request...
 def home(request):
-    # ...and renders the request along with the home.html template.
-    return render(request, "home.html")
+    # Collects all the different projects that exist.
+    projects = Project.objects.all()
+    # Collects all the different tags that exist.
+    tags = Tag.objects.all()
+    # ...and renders the request along with the home.html template, passing
+    # the template a dictionary containing the projects and tags.  This gives
+    # us access to the projects and tags variables from the home.html file.
+    return render(request, "home.html", {"projects": projects, "tags": tags})
 
 def contact(request):
     return render(request, "contact.html")
